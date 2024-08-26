a0121c7462d868bf1f2a3b586ed24cc6531e285a
Mark Glenn M. Gabuya <markglenn.g@gmail.com>
2024-08-26 23:39:39 +0800

    I added the new template in case of emergency.

---
+++ template_for_practice/main_template/3. exercise_from_conditionals.rb
@@ -0,0 +1,148 @@
+#* Exercise 1: Calling a method from another method.
+puts "\nCalling a method from another method: "
+
+#* Solution: Create a custom calculator.
+def add(a, b)
+  a + b
+end
+
+def sub(a, b)
+  a - b
+end
+
+def mul(a, b)
+  a * b
+end
+
+def div(a, b)
+  a / b
+end
+
+def custom_calculator(a, b, operation)
+
+end
+
+puts custom_calculator(3, 5, "add") # => 8
+puts custom_calculator(2, 5, "subtract") # => -13
+puts custom_calculator(3, 5, "multiply") # => 15
+puts custom_calculator(20, 4, "divide") # => 5
+puts
+
+
+# Solution suggested by Cody AI which is to use symbols instead of strings for operation names in the
+# custom_calculator method. This can improve performance and reduce the risk of typos:
+# Symbols are strings that you prepend a colon ":" instead of enclosing them with quotes.
+
+#* Solution: Use the "case" operation.
+def custom_calculator2(a, b, operation)
+
+end
+
+puts custom_calculator2(3, 5, "add") # => 8
+puts custom_calculator2(2, 5, "subtract") # => -13
+puts custom_calculator2(3, 5, "multiply") # => 15
+puts custom_calculator2(20, 4, "divide") # => 5
+puts
+
+
+#* Solution: Utilize the guard clauses to handle edge cases early or validation process then subsequently do the actual calculation via "case" operation.
+def custom_calculator3(a, b, operation)
+
+end
+
+puts custom_calculator3(3, 5, "add") # => 8
+puts custom_calculator3(2, 5, "subtract") # => -13
+puts custom_calculator3(3, 5, "multiply") # => 15
+puts custom_calculator3(20, 4, "divide") # => 5
+puts
+
+
+
+#* Exercise 2: Multiple Conditions.
+puts "\n\nMultiple Conditions: "
+
+# Define a divisible_by_three_and_four method that
+# accepts a number as its argument.
+# It should return true if the number is evenly divisible by both 3 and 4.
+# It should return false otherwise.
+
+#* Solution:
+def divisible_by_three_and_four(num)
+
+end
+
+puts divisible_by_three_and_four(3) # => false
+puts divisible_by_three_and_four(4) # => false
+puts divisible_by_three_and_four(12) # => true
+puts divisible_by_three_and_four(18) # => false
+puts divisible_by_three_and_four(24) # => true
+puts
+
+
+# Declare a string_theory function that accepts a string as an argument.
+# It should return true if the string has more than 4 characters
+# or if the string includes the character "B" (capital).
+
+#* Solution:
+def string_theory(string)
+
+end
+
+puts string_theory("Big Mac") # => true
+puts string_theory("Bank") # => true
+puts string_theory("refrigerator") # => true
+puts string_theory("boy") # => false
+puts string_theory("car") # => false
+
+
+
+#* Exercise 3: If-statements.
+puts "\n\nIf-Statements: "
+
+# Define a even_or_odd_operations method that accepts an integer.
+# If the integer is even, the method should return the number + 2
+# If the integer is odd, the method should return the number - 3
+
+#* Solution:
+def even_or_odd_operations(integer)
+
+end
+
+puts even_or_odd_operations(2) # => 4
+puts even_or_odd_operations(0) # => 2
+puts even_or_odd_operations(13) # => 10
+puts even_or_odd_operations(9) # => 6
+
+
+
+#* Exercise 4: If-Elsif-Else Statements.
+puts "\n\nIf-Elsif-Else Statements: "
+
+# Declare a numeric_energy function that accepts a number.
+# If the number is positive, return the string "Positive"
+# If the number is negative, return the string "Negative"
+# If the number is zero, return the string "Zero Hero"
+
+#* Solution:
+def numeric_energy(number)
+
+end
+
+puts numeric_energy(5) # => Positive
+puts numeric_energy(10) # => Positive
+puts numeric_energy(-5) # => Negative
+puts numeric_energy(-8) # => Negative
+puts numeric_energy(0) # => Zero Hero
+puts
+
+
+#* Solution: Solving Numeric Energy using "case method" for better readability.
+def numeric_energy(number)
+
+end
+
+puts numeric_energy(5) # => Positive
+puts numeric_energy(10) # => Positive
+puts numeric_energy(-5) # => Negative
+puts numeric_energy(-8) # => Negative
+puts numeric_energy(0) # => Zero Hero
---
+++ template_for_practice/main_template/6. exercises_from_arrays_I.rb
@@ -0,0 +1,71 @@
+#* Exercise 1: Indexing into arrays.
+puts "Array Indexing: "
+
+# Define a first_and_last method that accepts an array of strings.
+# The method should concatenate the first element and the last element
+# and return the result.
+# Assume the list will always have 1 or more elements.
+
+#* First and last elements:
+def first_and_last(string_array)
+
+end
+
+p first_and_last(["a", "b", "c"]) # => "ac"
+p first_and_last(["bob", "tom", "rob"]) # => "bobrob"
+p first_and_last(["a"]) # => "aa"
+puts
+
+
+# Define a product_of_even_indices method that accepts an
+# array of numbers. The array will always have 6 total elements.
+# The method should return the product (multiplied total) of
+# all numbers at an even index (0, 2, 4).
+
+#* Product of even indices:
+def product_of_even_indices(integer_array)
+
+end
+
+p product_of_even_indices([1, 2, 3, 4, 5, 6]) # => 15
+p product_of_even_indices([3, 4, 3, 5, 3, 6]) # => 27
+puts
+
+
+# Define a first_letter_of_last_string method that accepts an
+# array of strings. It should return one character: the first
+# letter of the last string in the array.
+# Assume the array will always have at least one string.
+
+#* First letter of last string:
+def first_letter_of_last_string(string_array)
+  # Using the 2D indexing:
+
+end
+
+p first_letter_of_last_string(["cat", "dog", "zebra"]) # => "z"
+p first_letter_of_last_string(["nonsense"])            # => "n"
+puts "\n\n"
+
+
+
+#* Exercise 2: Extracting multiple array elements.
+puts "Extracting Multiple Array Elements: "
+
+# Define a split_in_two method that accepts an array.
+# I'd like to split the array into two arrays.
+# If the original array has an even number of elements,
+# ensure that the 2 new arrays have an equal number of elements
+# If the original array has an odd number of elements,
+# ensure that the first new array has the greater number of elements.
+
+# Split in two:
+def split_in_two(array)
+
+end
+
+p split_in_two(["A", "B"]) # => [["A"], ["B"]]
+p split_in_two(["A", "B", "C", "D"]) # => [["A", "B"], ["C","D"]]
+p split_in_two(["A", "B", "C"]) # => [["A", "B"], ["C"]]
+p split_in_two(["A", "B", "C", "D", "E"]) # => [["A", "B", "C"],["D","E"]]
+puts

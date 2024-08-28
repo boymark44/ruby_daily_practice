85255fcf2d67055c91fb1f1a5358e8e4c76c7582
Mark Glenn M. Gabuya <markglenn.g@gmail.com>
2024-08-28 18:01:50 +0800

    Practiced the exercises from 1 to 4.

---
+++ day_4_practice/4. exercise_from_strings_I.rb
@@ -0,0 +1,165 @@
+#* Exercise 1: String Index Position.
+#* Methods: Bracket Syntax
+puts "\nString Index Position: "
+
+# Define a same_first_and_last_letter method that accepts a string.
+# The method should return true if the first and last character are equal,
+# and false otherwise.
+# Assume the string will always have 1 or more characters.
+
+#* Solution:
+def same_first_and_last_letter(string)
+  # Use bracket syntax to compare the first and last index of the string:
+  string[0] == string[-1]
+end
+
+puts same_first_and_last_letter("runner") # => true
+puts same_first_and_last_letter("Runner") # => false
+puts same_first_and_last_letter("clock") # => false
+puts same_first_and_last_letter("q") # => true
+puts
+
+
+# Define a three_number_sum method that accepts a 3-character string.
+# The method should calculate the sum of the digits of the string.
+# HINT: You’ll have to figure out a way to convert the
+# string-ified digits to integers.
+
+#* Solution:
+def three_number_sum(numbers)
+  # Access the index: 0, 1, and 2 then convert them into integers:
+  result = numbers[0].to_i + numbers[1].to_i + numbers[2].to_i
+
+  result
+end
+
+puts three_number_sum("123") # => 6
+puts three_number_sum("567") # => 18
+puts three_number_sum("444") # => 12
+puts three_number_sum("000") # => 0
+
+
+
+#* Exercise 2: String Slicing.
+#* Methods: Bracket Syntax
+puts "\n\nString Slicing: "
+
+# Define a first_three_characters method that accepts a string.
+# The method should return the first 3 characters of the string.
+
+#* Solution: Using bracket syntax.
+def first_three_characters(string)
+  string[0] + string[1] + string[2]
+end
+
+p first_three_characters("dynasty") # => dyn
+p first_three_characters("empire") # => emp
+puts
+
+
+#* Solution: Using the each_char method.
+def first_three_characters2(string)
+  # # 1st Approach:
+  # output = ""
+
+  # string.each_char do |character|
+  #   if output.length >= 3
+  #     output
+  #   else
+  #     output << character
+  #   end
+  # end
+
+  # output
+
+  output = ""
+  count = 0
+
+  string.each_char do |character|
+    output << character
+
+    if count == 2
+      break
+    else
+      count += 1
+    end
+  end
+
+  output
+end
+
+p first_three_characters2("dynasty") # => dyn
+p first_three_characters2("empire") # => emp
+puts
+
+
+# Define a five_from_the_end method that accepts a string.
+# The method should return the last 5 characters of the string.
+
+#* Solution: Using a bracket syntax.
+def five_from_the_end(string)
+  string[-5, 5]
+end
+
+p five_from_the_end("dynasty") # => nasty
+p five_from_the_end("rhinoceros") # => ceros
+puts
+
+
+# Using the slice method:
+def five_from_the_end(string)
+  string.slice(-5, 5)
+end
+
+puts five_from_the_end("dynasty") # => nasty
+puts five_from_the_end("rhinoceros") # => ceros
+
+
+
+#* Exercise 3: String Length and Comparison.
+#* Methods: length, size
+puts "\n\nString Length and Comparison: "
+
+# Define a long_word method that accepts a string.
+# The method should return a Boolean that reflects whether
+# the string has more than 7 characters.
+
+#* Solution: Kind of a lengthy approach.
+def long_word(string)
+  string_length = string.length
+
+  if string_length > 7
+    true
+  else
+    false
+  end
+end
+
+puts long_word("Ruby") # => false
+puts long_word("Magnificent") # => true
+puts
+
+
+#* Solution: The not so lengthy:
+def long_word1(string)
+  string.length > 7 ? true : false
+end
+
+puts long_word1("Ruby") # => false
+puts long_word1("Magnificent") # => true
+puts
+
+
+# Define a first_longer_than_second method that accepts two string arguments.
+# The method should return true if the first string is longer than the second
+# and false otherwise (including if they are equal in length).
+
+#* Solution:
+def first_longer_than_second(string1, string2)
+  string1.length > string2.length ? true : false
+end
+
+puts first_longer_than_second("Python", "Ruby") # => true
+puts first_longer_than_second("cat", "mouse") # => false
+puts first_longer_than_second("Steven", "Seagal") # => false
+puts

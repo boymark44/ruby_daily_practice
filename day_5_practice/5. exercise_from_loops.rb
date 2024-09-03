7ab787e31f906ac7203e15f25977b8c7c2dc78ef
Mark Glenn M. Gabuya <markglenn.g@gmail.com>
2024-09-03 18:14:38 +0800

    Finished the Exercise 5.

--- day_5_practice/5. exercise_from_loops.rb
+++ day_5_practice/5. exercise_from_loops.rb
@@ -69,11 +69,22 @@ end
 #* Exercise 3: The Break Keyword.
 puts "\n\nThe Break Keyword: "
 
-# Mission: Find the first iteration or existence of "$" from the given string and terminate the loop completely. Failure is not an option.
+#* Mission: Find the first iteration or existence of "$" from the given string and terminate the loop completely. Failure is not an option.
 money_sentence = "I love $ in the morning, $ in the afternoon, and $ in the evening."
 
-# Solution:
+#* Solution:
+current_index = 0
+final_index = money_sentence.length - 1
+first_money_index = nil
 
+while current_index <= final_index
+  if money_sentence[current_index] == "$"
+    first_money_index = current_index
+    break
+  end
+
+  current_index += 1
+end
 
 puts first_money_index # => 7
 
@@ -96,11 +107,10 @@ puts "\n\nRecursion: Factorial. "
 # Solution:
 def factorial(num)
   # Set the base case:
-
-
+  return num if num == 1
 
   # Recursive case:
-
+  num * factorial(num - 1)
 end
 
 puts factorial(4) # => 24
@@ -123,7 +133,17 @@ puts "\n\nRecursion: Reversing a String. "
 
 # Solution:
 def reverse_string(string)
+  reversed_text = ""
 
+  first_index = 0
+  current_num_from_the_rear = string.length - 1
+
+  while current_num_from_the_rear >= first_index
+    reversed_text << string[current_num_from_the_rear]
+    current_num_from_the_rear -= 1
+  end
+
+  reversed_text
 end
 
 puts reverse_string("straw hat") # => tah warts
@@ -139,7 +159,13 @@ puts
 
 # Solution:
 def reverse_string(string)
+  reversed_text = ""
 
+  string.each_char do |character|
+    reversed_text = character + reversed_text
+  end
+
+  reversed_text
 end
 
 puts reverse_string("straw hat") # => tah warts
@@ -154,10 +180,10 @@ puts
 # Solution:
 def reverse_string(string)
   # Base Case:
-
+  return string if string.length == 1
 
   # Recursive Case:
-
+  string[-1] + reverse_string(string[0...-1])
 end
 
 # Explanation:
@@ -191,7 +217,21 @@ puts "\n\nFizzBuzz Problem: "
 # For numbers divisible by both 3 and 5, print “FizzBuzz”. Otherwise, just print the number.
 
 def fizzbuzz(num)
+  count = 1
 
+  while count < num
+    if count % 3 == 0 and count % 5 == 0
+      puts "FizzBuzz"
+    elsif count % 3 == 0
+      puts "Fizz"
+    elsif count % 5 == 5
+      puts "Buzz"
+    else
+      puts count
+    end
+
+    count += 1
+  end
 end
 
 puts fizzbuzz(30)

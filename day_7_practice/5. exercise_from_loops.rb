7d9f09db14fe47ad515ee899cb54fb0e4e85b436
Mark Glenn M. Gabuya <markglenn.g@gmail.com>
2024-09-08 15:52:39 +0800

    Finished the Day 7 - Exercise 5.

--- day_7_practice/5. exercise_from_loops.rb
+++ day_7_practice/5. exercise_from_loops.rb
@@ -2,13 +2,25 @@
 puts "\nWhile Loop: "
 
 #* Solution: Implement a simple while loop that counts from 1 to 10:
+count = 1
 
+while count <= 10
+  puts count
 
+  count += 1
+end
+puts
+puts count
 puts
 
-
 #* Solution: Implement a simple while loop that appends the character "a" for every iteration. Do it in 5 iterations.
+character_count = "a"
 
+while character_count.length <= 5
+  puts character_count
+
+  character_count << "a"
+end
 
 
 
@@ -20,13 +32,36 @@ money_sentence = "I love $ in the morning, $ in the afternoon, and $ in the even
 
 
 #* Solution: Without using the next keyword.
+current_index = 0
+final_index = money_sentence.length - 1
 
+while current_index <= final_index
+  if money_sentence[current_index] == "$"
+    puts "Dollars is found at #{current_index}."
+  end
+
+  current_index += 1
+end
 
 puts
 
 
 #* Solution: Using the next keyword.
+# Iterate through each character of the string and return the index position in which the "$" is found
+money_sentence = "I love $ in the morning, $ in the afternoon, and $ in the evening."
 
+current_index = 0
+final_index = money_sentence.length - 1
+
+while current_index <= final_index
+  if money_sentence[current_index] != "$"
+    current_index += 1
+    next
+  end
+
+  puts "Dollars is found at #{current_index}."
+  current_index += 1
+end
 
 
 
@@ -37,6 +72,18 @@ puts "\n\nThe Break Keyword: "
 money_sentence = "I love $ in the morning, $ in the afternoon, and $ in the evening."
 
 #* Solution:
+current_index = 0
+final_index = money_sentence.length - 1
+first_money_index = nil
+
+while current_index <= final_index
+  if money_sentence[current_index] == "$"
+    first_money_index = current_index
+    break
+  else
+    current_index += 1
+  end
+end
 
 
 puts first_money_index # => 7
@@ -60,11 +107,10 @@ puts "\n\nRecursion: Factorial. "
 #* Solution:
 def factorial(num)
   # Set the base case:
-
-
+  return 1 if num == 1
 
   # Recursive case:
-
+  num * factorial(num - 1)
 end
 
 puts factorial(4) # => 24
@@ -88,6 +134,16 @@ puts "\n\nRecursion: Reversing a String. "
 #* Solution:
 def reverse_string(string)
 
+  first_index = 0
+  current_index_from_rear = string.length - 1
+  reversed_text = ""
+
+  while current_index_from_rear >= first_index
+    reversed_text << string[current_index_from_rear]
+    current_index_from_rear -= 1
+  end
+
+  reversed_text
 end
 
 puts reverse_string("straw hat") # => tah warts
@@ -104,6 +160,13 @@ puts
 #* Solution:
 def reverse_string(string)
 
+  reversed_text = ""
+
+  string.each_char do |character|
+    reversed_text = character + reversed_text
+  end
+
+  reversed_text
 end
 
 puts reverse_string("straw hat") # => tah warts
@@ -118,10 +181,10 @@ puts
 #* Solution:
 def reverse_string(string)
   # Base Case:
-
+  return string if string.length <= 1
 
   # Recursive Case:
-
+  string[-1] + reverse_string(string[0...-1])
 end
 
 # Explanation:
@@ -156,6 +219,20 @@ puts "\n\nFizzBuzz Problem: "
 
 def fizzbuzz(num)
 
+  count = 1
+
+  while count <= num
+    if count % 3 == 0 and count % 5 == 0
+      puts "FizzBuzz"
+    elsif count % 3 == 0
+      puts "Fizz"
+    elsif count % 5 == 0
+      puts "Buzz"
+    else
+      puts count
+    end
+    count += 1
+  end
 end
 
 puts fizzbuzz(30)

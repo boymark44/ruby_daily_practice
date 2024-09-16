0026075e399b2fff5d43b1eb4402aba47dd26a9b
Mark Glenn M. Gabuya <markglenn.g@gmail.com>
2024-09-16 13:31:55 +0800

    Starting the Day 11 Ruby Exercises Practice.

---
+++ day_11_practice/1. exercise_from_methods.rb
@@ -0,0 +1,24 @@
+#* Exercise 1: Optional Parameters and Default Arguments.
+
+#* Solution: Calculate the total meal cost with the default tip percentage of 20% or 0.20
+def calculate_meal_cost(total_amount, tip_percentage = 0.2)
+
+end
+
+puts calculate_meal_cost(20, 0.05) # => $21.0
+puts calculate_meal_cost(20) # => $24.0
+puts calculate_meal_cost(100, 0.12) # => $112.0
+puts calculate_meal_cost(100) # => $120.0
+puts
+
+
+#* Solution:
+def string_adder(a = "", b = "")
+  # Interpolate a and b:
+
+end
+
+puts string_adder("Hello", "World") # => Hello World
+puts string_adder("Emilio", "Esteves") # => Emilio Esteves
+p string_adder # => " "
+puts string_adder("Tiger") # => Tiger

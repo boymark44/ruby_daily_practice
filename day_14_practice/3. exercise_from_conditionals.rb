428ea188b6addf1e6714e3acd626b6e729156c59
Mark Glenn M. Gabuya <markglenn.g@gmail.com>
2024-12-23 17:16:30 +0800

    Finished practicing the third exercise which is the ruby conditionals to understand how to make conditionals in ruby

--- day_14_practice/3. exercise_from_conditionals.rb
+++ day_14_practice/3. exercise_from_conditionals.rb
@@ -21,6 +21,22 @@ end
 #* Solution: Use the if-elsif-else statements.
 def custom_calculator(a, b, operation)
 
+  if operation == "add"
+    add(a, b)
+  elsif operation == "subtract"
+    sub(a, b)
+  elsif operation == "multiply"
+    mul(a, b)
+  elsif operation == "divide"
+    if b.zero?
+      "Cannot be divided by zero"
+    else
+      div(a, b)
+    end
+  else 
+    "Invalid Operation"
+  end
+
 end
 
 puts custom_calculator(3, 5, "add") # => 8
@@ -38,6 +54,14 @@ puts
 
 #* Solution: Use the "case" operation.
 def custom_calculator2(a, b, operation)
+  
+  case operation
+  when :add then add(a, b)
+  when :subtract then sub(a, b)
+  when :multiply then mul(a, b)
+  when :divide then b.zero? ? "Cannot be divided by zero" : div(a, b)
+  else "Invalid Operation"
+  end
 
 end
 
@@ -53,6 +77,15 @@ puts
 #* Solution: Utilize the guard clauses to handle edge cases early or validation process then subsequently do the actual calculation via "case" operation.
 def custom_calculator3(a, b, operation)
 
+  return "Invalid Operation" unless ["add", "subtract", "multiply", "divide"].include?(operation)
+  return "Cannot be divided by zero" if operation == "divide" and b.zero?
+
+  case operation
+  when "add" then add(a, b)
+  when "subtract" then sub(a, b)
+  when "multiply" then mul(a, b)
+  when "divide" then div(a, b)
+  end
 end
 
 puts custom_calculator3(3, 5, "add") # => 8
@@ -75,7 +108,7 @@ puts "\n\nMultiple Conditions: "
 
 #* Solution:
 def divisible_by_three_and_four(num)
-
+  num % 3 == 0 and num % 4 == 0 
 end
 
 puts divisible_by_three_and_four(3) # => false
@@ -92,7 +125,7 @@ puts
 
 #* Solution:
 def string_theory(string)
-
+  string.include?("B") or string.length > 4
 end
 
 puts string_theory("Big Mac") # => true
@@ -112,7 +145,11 @@ puts "\n\nIf-Statements: "
 
 #* Solution:
 def even_or_odd_operations(integer)
-
+  if integer.even? 
+    integer + 2
+  else 
+    integer - 3 
+  end
 end
 
 puts even_or_odd_operations(2) # => 4
@@ -132,7 +169,13 @@ puts "\n\nIf-Elsif-Else Statements: "
 
 #* Solution:
 def numeric_energy(number)
-
+  if number > 0
+    "Positive"
+  elsif number < 0 
+    "Negative"
+  else 
+    "Zero Hero"
+  end
 end
 
 puts numeric_energy(5) # => Positive
@@ -145,7 +188,11 @@ puts
 
 #* Solution: Solving Numeric Energy using "case method" for better readability.
 def numeric_energy(number)
-
+  case 
+  when number.positive? then "Positive"
+  when number.negative? then "Negative"
+  else "Zero Hero"
+  end
 end
 
 puts numeric_energy(5) # => Positive

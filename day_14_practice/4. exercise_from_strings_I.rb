2f4439a644554463abf999790b0385d0ab946705
Mark Glenn M. Gabuya <markglenn.g@gmail.com>
2024-12-24 13:13:12 +0800

    Finished the fourth exercise which is ruby strings.

--- day_14_practice/4. exercise_from_strings_I.rb
+++ day_14_practice/4. exercise_from_strings_I.rb
@@ -8,9 +8,7 @@ puts "\nString Index Position: "
 
 #* Solution: Using the Bracket Syntax.
 def same_first_and_last_letter(string)
-
-
-
+    string[0] == string[-1]
 end
 
 puts same_first_and_last_letter("runner") # => true
@@ -27,8 +25,11 @@ puts
 
 #* Solution: Extract elements from index: 0, 1, and 2 then convert them into integers.
 def three_number_sum(numbers)
+    rolling_sum = 0 
 
+    numbers.each_char { |num| rolling_sum += num.to_i }
 
+    rolling_sum
 end
 
 puts three_number_sum("123") # => 6
@@ -46,7 +47,7 @@ puts "\n\nString Slicing: "
 
 #* Solution: Using bracket syntax.
 def first_three_characters(string)
-
+    string[0] + string[1] + string[2]
 end
 
 puts first_three_characters("dynasty") # => dyn
@@ -56,7 +57,11 @@ puts
 
 #* Solution: Using the each_char method.
 def first_three_characters2(string)
+    result_string = ""
 
+    string.each_char { |char| result_string << char unless result_string.length == 3 }
+
+    result_string
 end
 
 puts first_three_characters2("dynasty") # => dyn
@@ -68,8 +73,8 @@ puts
 # The method should return the last 5 characters of the string.
 
 #* Solution: Using a bracket syntax.
-def five_from_the_end(string)
-
+def five_from_the_end(string)   
+    string[-5, 5]
 end
 
 puts five_from_the_end("dynasty") # => nasty
@@ -79,7 +84,7 @@ puts
 
 # Using the slice method:
 def five_from_the_end(string)
-
+    string.slice(-5, 5)
 end
 
 puts five_from_the_end("dynasty") # => nasty
@@ -96,7 +101,7 @@ puts "\n\nString Length and Comparison: "
 
 #* Solution:
 def long_word1(string)
-
+    string.length > 7
 end
 
 puts long_word1("Ruby") # => false
@@ -110,7 +115,7 @@ puts
 
 #* Solution:
 def first_longer_than_second(string1, string2)
-
+    string1.length >= string2.length 
 end
 
 puts first_longer_than_second("Python", "Ruby") # => true

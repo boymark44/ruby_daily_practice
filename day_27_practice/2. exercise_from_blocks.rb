9f6141d1d8fb6d31a68e1752f0de09f3f26a6a34
Lexitect <lexitech@Lexitects-MacBook-Air.local>
2025-04-06 14:14:11 +0800

    Finished the exercise from blocks.

--- day_27_practice/2. exercise_from_blocks.rb
+++ day_27_practice/2. exercise_from_blocks.rb
@@ -3,7 +3,9 @@ puts "\nImplement a block: "
 
 
 #* Solution: Print "Hello" 5 times.
-
+def print_five_times
+  5.times { print "Hello" }
+end
 
 
 print_five_times # => HelloHelloHelloHelloHello
@@ -12,8 +14,9 @@ puts
 
 
 #* Solution: Use the "num" as the number of times "Money" should be printed:
-
-
+def money_printer(num)
+  num.times { print "Money" }
+end
 
 money_printer(3) # => MoneyMoneyMoney
 money_printer(5) # => MoneyMoneyMoneyMoneyMoney
@@ -25,11 +28,13 @@ p money_printer(0) # => ["", nil]
 puts "\n\nUsing a range, step, to_a, and join: "
 
 #* Solution: Combine all the methods above to generate the answer.
-p # => "0246810"
+p (0..10).step(2).to_a.join # => "0246810"
 
 
 #* Solution: Use .step method to print even elements in a single-line block.
-
+def increments_of_two
+  0.step(10, 2) { |num| print num }
+end
 
 
 increments_of_two # => 0246810
@@ -37,22 +42,32 @@ puts
 
 
 #* Solution: Use the .upto method to print even elements in a single-line block.
+def increments_of_two
+  0.upto(10) { |num| print num if num.even? }
+end
 
 
-
-increments_of_two # => 0246810
+puts increments_of_two # => 0246810
 puts
 
 
 #* Solution: Using an empty string to print the even elements.
+def increments_of_two
+  result_string = "" 
 
+  0.upto(10) { |num| result_string << num.to_s if num.even? }
+
+  result_string
+end
 
 
 puts increments_of_two # => 0246810
 
 
 #* Solution from Boris: Use the times method 6-times.
-
+def increments_of_two
+  6.times { |num| print num * 2 }
+end
 
 
 increments_of_two # => 0246810
